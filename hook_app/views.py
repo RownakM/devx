57e936f3c80e450c85b082ffd9e6a468c7eac26e
RownakM <rmazumder1033@gmail.com>
2023-02-25 20:16:28 +0530

    CSRF

--- hook_app/views.py
+++ hook_app/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import git,os
+from django.views.decorators.csrf import csrf_exempt
 
 from devx.settings import BASE_DIR
 # Create your views here.
+@csrf_exempt
 def pullGithub(request):
     # if request.method=="POST":
     repo=git.Repo(BASE_DIR)
